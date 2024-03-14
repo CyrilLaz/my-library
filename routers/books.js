9adbc314cabac6348839e9c976269417f017f30a
Cyril_Lazar <klazar@mail.ru>
2024-03-15 00:45:04 +0700

    fix

--- routers/books.js
+++ routers/books.js
@@ -6,14 +6,14 @@ const {
   deleteBook,
   downloadBookById,
 } = require("../controllers/books");
-const getFile = require("../middlewires/file");
+const uploadFile = require("../middlewires/file");
 
 const router = require("express").Router();
 
 router.get("/", getAllBooks);
 router.get("/:id", getBookById);
 router.get("/:id/download", downloadBookById);
-router.post("/", getFile.single("book-file"), createBook);
+router.post("/", uploadFile.single("book-file"), createBook);
 router.put("/:id", editBook);
 router.delete("/:id", deleteBook);
 
