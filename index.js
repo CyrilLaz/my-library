4dfed58ba25db6004aa213a4d6009fd106bc4f8f
Cyril_Lazar <klazar@mail.ru>
2024-03-23 02:05:44 +0700

    сделать logo

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 "use strict";
 const express = require("express");
-const fs = require('fs')
+const fs = require("fs");
 const { routers } = require("./routers");
-const { DEV_MODE, PORT,UPLOAD_FOLDER } = require("./config");
+const { DEV_MODE, PORT, UPLOAD_FOLDER } = require("./config");
 const { db } = require("./middlewares/db-middleware");
 const app = express();
-fs.mkdirSync(UPLOAD_FOLDER,{recursive: true})
+
+fs.mkdirSync(UPLOAD_FOLDER, { recursive: true });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/public',express.static("public"));
 app.set("view engine", "ejs");
-
 app.use("/", db, routers);
 
 // app.use(handleErrors);
